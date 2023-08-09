485702de4d66184496792d7ee15f73e288f19964
SovetovCumminist <89398225+SovetovCumminist@users.noreply.github.com>
2023-08-09 15:23:35 +0300

    start

---
+++ common/defines/Misato.lua
@@ -0,0 +1,14 @@
+NDefines.NGame.COMBAT_LOG_MAX_MONTHS = 1
+NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 30
+NDefines.NGame.LAG_DAYS_FOR_PAUSE = 20
+NDefines.NGame.GAME_SPEED_SECONDS = { 1.0, 0.25, 0.1, 0.05, 0.0 }
+NDefines.NCountry.EVENT_PROCESS_OFFSET = 30
+NDefines_Graphics.NGraphics.MAPICON_GROUP_PASSES = 10
+
+NDefines.NAI.DIPLOMATIC_ACTION_RANDOM_FACTOR = 0.0
+
+NDefines_Graphics.NGraphics.CAPITAL_ICON_CUTOFF = 800
+NDefines_Graphics.NGraphics.DRAW_MAP_OBJECTS_CUTOFF = 250
+NDefines_Graphics.NGraphics.DRAW_FOW_CUTOFF = 0
+NDefines_Graphics.NGraphics.DRAW_FOW_FADE_LENGTH = 0
+NDefines_Graphics.NGraphics.MAP_BUILDINGS_SHRINK_DISTANCE = 100
---
+++ descriptor.mod
@@ -0,0 +1,7 @@
+version="1.0"
+tags={
+	"Alternative History"
+}
+name="Yor custom 09.08.2023"
+picture="picture.png"
+supported_version="1.12.*"
